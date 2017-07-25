acffc258e3abc59dfeeeef5c0ae02b6df2bbd92a
pajanne <pajanne@gmail.com>
2017-07-25 12:43:27 +0100

    update project details, issues with special characters in experimental details field, not loaded yet

---
+++ update_projects.py
@@ -0,0 +1,29 @@
+import sqlalchemy
+from datetime import datetime
+import csv
+
+from proteomicsdb.model import Base
+from proteomicsdb.model import Project
+from config import DATABASE_URI
+
+engine = sqlalchemy.create_engine(DATABASE_URI)
+Base.metadata.bind = engine
+dbsession = sqlalchemy.orm.sessionmaker(bind=engine)
+dbsession = dbsession()
+
+with open('update_project.txt', 'rU') as f:
+    reader = csv.DictReader(f, delimiter='\t')
+    for line in reader:
+        project = dbsession.query(Project).filter(Project.name == line['name']).one()
+        if project:
+            project.completion_date = datetime.strptime(line['completion_date'], '%d.%m.%y')
+            project.description = line['description']
+            project.experiment_type = line['experiment_type']
+            project.researcher = line['researcher']
+            project.research_group = line['research_group']
+            project.cell_tissue_type = line['cell_tissue_type']
+            project.species = line['species']
+            project.instrument = line['instrument']
+            print project.name, 'updated'
+
+dbsession.commit()
